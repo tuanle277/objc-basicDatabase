d99f3e49c2c688541128e122586403edc4c9ddb6
tuanle277 <tuanle_2025@depauw.edu>
2022-06-09 20:49:18 +0700

    Enter

--- progClass/tableCell.m
+++ progClass/tableCell.m
@@ -6,8 +6,8 @@
 //like constructor but assign model's properties into cell's properties
 -(void) setUp: (Customer* )customer withIndexPath: (NSIndexPath *) indexPath {
     self.nameLabel.text = [NSString stringWithFormat: @"Name: %@", customer.username];
-    self.mailLabel.text = [NSString stringWithFormat: @"Name: %@", customer.email];
-    self.ageLabel.text = [NSString stringWithFormat: @"Name: %d", customer.age];
+    self.mailLabel.text = [NSString stringWithFormat: @"Email: %@", customer.email];
+    self.ageLabel.text = [NSString stringWithFormat: @"Age: %d", customer.age];
     self.phoneLabel.text = [NSString stringWithFormat: @"Phone number: %@", customer.phone];
 //    self.image.image = [UIImage imageNamed: customer.profile];
 }
